e4c0576aa8da1cd4989264bd62d2cee3146e626d
Andrew-van-Campen <andrewvancampen@gmail.com>
2025-07-09 14:13:10 -0400

    Added more settings

--- init.lua
+++ init.lua
@@ -1,9 +1,16 @@
 
 vim.opt.number = true  -- Show line numbers
+vim.opt.list = true  -- Show trailing whitespace characters
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }  -- Set whitespace characters
+vim.opt.cursorline = true  -- Highlight current line
 
 -- Set tabs/indentation
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.cmd('autocmd FileType * set expandtab')
+vim.cmd('autocmd FileType * set shiftwidth=4')
+vim.cmd('autocmd FileType * set tabstop=4')
+vim.cmd('autocmd FileType * set softtabstop=4')
+vim.cmd('autocmd FileType * set cindent')
+
+vim.cmd('inoremap {<Enter> {<Enter>}<Esc>O')  -- Auto-close braces for blocks
+vim.cmd('autocmd FileType * set formatoptions-=cro')  -- Don't auto comment
 
