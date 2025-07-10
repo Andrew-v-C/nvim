3bf66bc063eb65ced2bf80ee84d1bc717da435fc
Andrew-van-Campen <andrewvancampen@gmail.com>
2025-07-10 14:35:41 -0400

    Split config into multiple files

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 
+require("options")
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -26,7 +28,12 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
     spec = {
         -- Add your plugins here
-        "rebelot/kanagawa.nvim"
+        {
+            "rebelot/kanagawa.nvim",
+            config = function()
+                vim.cmd("colorscheme kanagawa")  -- Set color scheme
+            end,
+        },
     },
     -- Configure any other settings here; see the documentation for more details
     -- Colorscheme that will be used when installing plugins
@@ -34,26 +41,4 @@ require("lazy").setup({
     -- Automatically check for plugin updates
     checker = { enabled = true },
 })
-
-vim.opt.number = true  -- Show line numbers
---vim.opt.relativenumber = true -- Enable relative line numbers
-vim.opt.list = true  -- Show trailing whitespace characters
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }  -- Set whitespace characters
-vim.opt.cursorline = true  -- Highlight current line
-vim.opt.splitbelow = true  -- Open new windows below
-vim.opt.splitright = true  -- Open new windows to the right
-vim.opt.scrolloff = 999  -- Set scrolling behaviour
-vim.opt.termguicolors = true  -- Enable 24-bit color in TUI
-vim.cmd("colorscheme kanagawa")  -- Set color scheme
-
--- Set tabs/indentation
-vim.cmd("autocmd FileType * set expandtab")
-vim.cmd("autocmd FileType * set shiftwidth=4")
-vim.cmd("autocmd FileType * set tabstop=4")
-vim.cmd("autocmd FileType * set softtabstop=4")
-vim.cmd("autocmd FileType * set cindent")
-
-vim.cmd("inoremap {<Enter> {<Enter>}<Esc>O")  -- Auto-close braces for blocks
-vim.cmd("autocmd FileType * set formatoptions-=cro")  -- Don't auto comment
-vim.opt.virtualedit = "block"  -- Use virtual edit in visual block mode
 
---
+++ options.lua
@@ -0,0 +1,22 @@
+
+vim.opt.number = true  -- Show line numbers
+--vim.opt.relativenumber = true -- Enable relative line numbers
+vim.opt.list = true  -- Show trailing whitespace characters
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }  -- Set whitespace characters
+vim.opt.cursorline = true  -- Highlight current line
+vim.opt.splitbelow = true  -- Open new windows below
+vim.opt.splitright = true  -- Open new windows to the right
+vim.opt.scrolloff = 999  -- Set scrolling behaviour
+vim.opt.termguicolors = true  -- Enable 24-bit color in TUI
+
+-- Set tabs/indentation
+vim.cmd("autocmd FileType * set expandtab")
+vim.cmd("autocmd FileType * set shiftwidth=4")
+vim.cmd("autocmd FileType * set tabstop=4")
+vim.cmd("autocmd FileType * set softtabstop=4")
+vim.cmd("autocmd FileType * set cindent")
+
+vim.cmd("inoremap {<Enter> {<Enter>}<Esc>O")  -- Auto-close braces for blocks
+vim.cmd("autocmd FileType * set formatoptions-=cro")  -- Don't auto comment
+vim.opt.virtualedit = "block"  -- Use virtual edit in visual block mode
+
