add7f9756e9b199bf98743d6f2f95f983793e22c
Andrew-v-C <andrewvancampen@gmail.com>
2025-07-23 18:13:09 -0400

    Created separate file for custom functions

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 require("autocommands")
 require("options")
 require("plugins")
+require("functions")
 
 -- Edit this depending on the system
 -- vim.opt.shell = "pwsh"  -- Shell to use in terminal mode
