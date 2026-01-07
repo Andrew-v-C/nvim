05792b31168eb952988df29430cfbf9bc7bb57cf
Andrew-v-C <andrewvancampen@gmail.com>
2026-01-07 11:38:54 -0500

    Adjusted keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,9 @@
 
-vim.keymap.set("n", "<F2>", "viw<C-G>")  -- F2 selects word under cursor
-vim.keymap.set("n", "<F4>", function() vim.cmd("edit .") end)  -- F4 opens file explorer
-vim.keymap.set("n", "<F5>", function() vim.cmd("mode|nohlsearch") end)  -- F5 clears and redraws screen
-vim.keymap.set("n", "<F6>", function() vim.lsp.buf.code_action() end)  -- F6 performs code action
-vim.keymap.set("n", "<F7>", function() vim.cmd("set spell!") end)  -- F7 toggles spell check
-vim.keymap.set("n", "<F12>", ":horizontal terminal<Enter>i")  -- F12 opens terminal window
 vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")  -- Make Esc behaviour consistent in terminal mode
+vim.keymap.set("n", "<F2>", "viw<C-G>")  -- F2 selects word under cursor
+vim.keymap.set("n", "<F3>", function() vim.cmd("edit .") end)  -- F3 opens file explorer
+vim.keymap.set("n", "<F5>", function() vim.cmd("mode|nohlsearch") end)  -- F5 clears and redraws screen
+vim.keymap.set("n", "<F7>", function() vim.cmd("set spell!") end)  -- F7 toggles spell check
+vim.keymap.set("n", "<F8>", function() vim.lsp.buf.code_action() end)  -- F8 performs code action
+vim.keymap.set("n", "<F12>", ":horizontal terminal<Enter>i")  -- F12 opens terminal window
 
