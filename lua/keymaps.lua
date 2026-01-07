2d40e5f481795cac0e470e9c4565d504dcde8785
Andrew-v-C <andrewvancampen@gmail.com>
2026-01-06 20:15:08 -0500

    Updated key mappings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,8 +1,9 @@
 
--- Custom key mappings / macros
-vim.keymap.set("n", "<F2>", "viw<C-G>")  -- F2 selects the word under the cursor
-vim.keymap.set("n", "<F4>", function() vim.cmd("edit .") end)  -- F4 opens the file explorer
+vim.keymap.set("n", "<F2>", "viw<C-G>")  -- F2 selects word under cursor
+vim.keymap.set("n", "<F4>", function() vim.cmd("edit .") end)  -- F4 opens file explorer
+vim.keymap.set("n", "<F5>", function() vim.cmd("mode|nohlsearch") end)  -- F5 clears and redraws screen
+vim.keymap.set("n", "<F6>", function() vim.lsp.buf.code_action() end)  -- F6 performs code action
 vim.keymap.set("n", "<F7>", function() vim.cmd("set spell!") end)  -- F7 toggles spell check
-vim.keymap.set("n", "<F12>", ":horizontal terminal<Enter>i")  -- F12 enters terminal mode
+vim.keymap.set("n", "<F12>", ":horizontal terminal<Enter>i")  -- F12 opens terminal window
 vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")  -- Make Esc behaviour consistent in terminal mode
 
