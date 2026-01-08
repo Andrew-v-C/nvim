bc6eadf2263d7c6c393cd3100a4c0494da05086f
Andrew-v-C <andrewvancampen@gmail.com>
2026-01-08 12:23:36 -0500

    Changed key mappings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,9 @@
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")  -- Make Esc consistent in terminal mode
-vim.keymap.set("n", "<F4>", function() vim.lsp.buf.code_action() end)  -- Perform code action
-vim.keymap.set("n", "<F7>", function() vim.cmd("set spell!") end)  -- Toggle spell check
-vim.keymap.set("n", "<F12>", ":horizontal terminal<Enter>i")  -- Open terminal window (horizontal)
-vim.keymap.set("n", "<C-F12>", ":vertical terminal<Enter>i")  -- Open terminal window (vertical)
+
+vim.g.mapleader = " "  -- Set leader key
+vim.keymap.set("n", "<Leader>4", function() vim.lsp.buf.code_action() end)  -- Perform code action
+vim.keymap.set("n", "<Leader>7", function() vim.cmd("set spell!") end)  -- Toggle spell check
+vim.keymap.set("n", "<Leader>9", ":horizontal terminal<Enter>i")  -- Open terminal window (horizontal)
+vim.keymap.set("n", "<Leader>0", ":vertical terminal<Enter>i")  -- Open terminal window (vertical)
 
