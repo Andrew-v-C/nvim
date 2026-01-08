d14aaf59a1e56742336f11bc9461886e8cef0184
Andrew-v-C <andrewvancampen@gmail.com>
2026-01-08 18:55:40 -0500

    Minimized key mappings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,4 @@
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")  -- Make Esc consistent in terminal mode
-
-vim.g.mapleader = " "  -- Set leader key
-vim.keymap.set("n", "<Leader>4", function() vim.lsp.buf.code_action() end)  -- Perform code action
-vim.keymap.set("n", "<Leader>7", function() vim.cmd("set spell!") end)  -- Toggle spell check
-vim.keymap.set("n", "<Leader>9", ":horizontal terminal<Enter>i")  -- Open terminal window (horizontal)
-vim.keymap.set("n", "<Leader>0", ":vertical terminal<Enter>i")  -- Open terminal window (vertical)
+vim.keymap.set("n", "<M-CR>", function() vim.lsp.buf.code_action() end)  -- Perform code action
 
