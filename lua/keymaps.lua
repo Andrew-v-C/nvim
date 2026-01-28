f7caf5f5e7de368ca23ece2bbfc14d603814c9c3
Andrew-v-C <andrewvancampen@gmail.com>
2026-01-28 09:50:35 -0500

    Disabled indenting for markdown files

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")                             -- Make Esc consistent in terminal mode
 vim.keymap.set("n", "<M-CR>", function() vim.lsp.buf.code_action() end) -- Perform code action
 vim.keymap.set("n", "<F7>", function() vim.cmd("set spell!") end)       -- Toggle spell check
+vim.keymap.set("n", "=", "<NOP>")                                       -- Disable = key in normal mode
