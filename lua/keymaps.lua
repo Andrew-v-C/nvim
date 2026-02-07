1f2e869b3387f9fbaa16a66497511adde0819741
Andrew-v-C <andrewvancampen@gmail.com>
2026-02-07 16:13:58 -0500

    Added key binding to run Python file

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,20 @@
-vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")                              -- Make Esc consistent in terminal mode
-vim.keymap.set("n", "<M-CR>", function() vim.lsp.buf.code_action() end)  -- Perform code action
-vim.keymap.set("n", "<F7>", function() vim.cmd("set spell!") end)        -- Toggle spell check
-vim.keymap.set("n", "=", "<NOP>")                                        -- Disable = key in normal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")  -- Make Esc consistent in terminal mode
+vim.keymap.set("n", "=", "<NOP>")            -- Disable = key in normal mode
+
+vim.keymap.set("n", "<F5>", function()
+    local ftype = vim.bo.filetype
+    if ftype == "python" then
+        -- Run the current Python file
+        vim.cmd("!py %")
+    end
+end)
+
+vim.keymap.set("n", "<F7>", function()
+    -- Toggle spell check
+    vim.cmd("set spell!")
+end)
+
+vim.keymap.set("n", "<M-CR>", function()
+    -- Perform code action
+    vim.lsp.buf.code_action()
+end)
