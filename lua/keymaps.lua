cf7db66a8729ef820c6e66c2a7a99a6b6212daac
Andrew-v-C <andrewvancampen@gmail.com>
2026-04-25 13:35:18 -0400

    Updated key maps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,15 +1,18 @@
+-- Function keys (Normal mode)
+vim.keymap.set("n", "<F3>", function() vim.cmd("edit .") end)          -- Open file explorer
+vim.keymap.set("n", "<F4>", ":vert term<Enter>i")                      -- Open terminal in vertical split
+vim.keymap.set("n", "<F6>", function() vim.lsp.buf.code_action() end)  -- Perform code action
+vim.keymap.set("n", "<F7>", function() vim.cmd("set spell!") end)      -- Toggle spell check
+vim.keymap.set("n", "<F8>", function()                                 -- Toggle highlighted column at 80 characters
+    if vim.api.nvim_get_option_value("colorcolumn", {}) == "80" then
+        vim.opt.colorcolumn = ""
+    else
+        vim.opt.colorcolumn = "80"
+    end
+end)
+
 -- Normal mode
 vim.keymap.set("n", "=", "<NOP>")  -- Disable "=" key
-
-vim.keymap.set("n", "<F7>", function()
-    -- Toggle spell check
-    vim.cmd("set spell!")
-end)
-
-vim.keymap.set("n", "<M-CR>", function()
-    -- Perform code action
-    vim.lsp.buf.code_action()
-end)
 
 -- Terminal mode
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")       -- Make Esc consistent
