9fad19ad138256abc1dccde299a23eb3cec10245
Andrew-v-C <andrewvancampen@gmail.com>
2026-04-25 14:29:45 -0400

    Fixed LSP issue with Arduino/C++

--- lsp/arduino-language-server.lua
+++ lsp/arduino-language-server.lua
@@ -6,9 +6,18 @@ else
 end
 
 return {
-    cmd = { "arduino-language-server", "-cli-config", configFile, },
-    filetypes = { "arduino" },
-    root_dir = vim.fn.expand("%:p:h"),
+    cmd = {
+        "arduino-language-server",
+        "-clangd", "clangd",
+        "-cli", "arduino-cli",
+        "-cli-config", configFile,
+    },
+    filetypes = { "arduino", "cpp", "c" },
+    root_dir = function(bufnr, cb)
+        local fname = vim.api.nvim_buf_get_name(bufnr)
+        local root = vim.fs.root(fname, { "sketch.yaml" })
+        if root then cb(root) end
+    end,
     capabilities = {
         textDocument = { semanticTokens = vim.NIL },
         workspace = { semanticTokens = vim.NIL },
--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,5 +1,15 @@
 return {
     cmd = { "clangd" },
     filetypes = { "c", "cpp" },
-    root_markers = { ".git" },
+    root_dir = function(bufnr, cb)
+        local fname = vim.api.nvim_buf_get_name(bufnr)
+        if vim.fs.root(fname, { "sketch.yaml" }) then return end
+        local root = vim.fs.root(fname, {
+            ".clangd",
+            "CMakeLists.txt",
+            "Makefile",
+            "compile_commands.json",
+        })
+        if root then cb(root) end
+    end,
 }
